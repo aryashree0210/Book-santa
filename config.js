68c9de6d7e3bdf7b28ad0b6991d8165d378d88fd
aryashree0210 <70515988+aryashree0210@users.noreply.github.com>
2021-05-21 11:05:08 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCJ2NVUh82cSkSVfqrqzAmdyqMn2qJoQEQ",
+  authDomain: "virtual-pet-20.firebaseapp.com",
+  databaseURL: "https://virtual-pet-20-default-rtdb.firebaseio.com",
+  projectId: "virtual-pet-20",
+  storageBucket: "virtual-pet-20.appspot.com",
+  messagingSenderId: "7304122679",
+  appId: "1:7304122679:web:e1e9c08b6e9adb32580ab2"
+};
+
+
+
+
+
+
+
+  if(!firebase.apps.length)
+  {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+
+export default firebase.firestore();
